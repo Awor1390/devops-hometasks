382c22377cc62a821597071262c6bbd583aaf358
Konstantin <pwd1390@mail.ru>
2024-03-07 18:35:35 +0300

    TF2

--- 13-terraform/wcg-git-state.tf
+++ 13-terraform/wcg-git-state.tf
@@ -1,29 +1,10 @@
-resource "github_repository" "devops-hometasks" {
-  name        = "devops-hometasks"
-  description = "Awor devops-hometasks repository"
-  auto_init   = false
-  visibility  = "public"
-}
-
-resource "github_branch" "master" {
-  repository = github_repository.devops-hometasks.name
-  branch     = "master"
-}
-
-resource "github_branch_default" "devops-hometasks-master" {
-  repository = github_repository.devops-hometasks.name
-  branch     = github_branch.master.branch
-}
-
-resource "github_repository_file" "file" {
-  repository = github_repository.devops-hometasks.name
-  branch     = github_branch.master.branch
-  file       = "test"
-  content    = "This is first file"
-
-  commit_author  = "Awor"
-  commit_email   = "pwd1390@mail.ru"
-  commit_message = "save TF state"
-
+resource "github_repository_file" "devops-hometasks" {
+  repository          = "devops-hometasks/13-terraform"
+  branch              = "master"
+  file                = "terraform.tfstate"
+  content             = file("terraform.tfstate")
+  commit_message      = "save TF state"
+  commit_author       = "Awor"
+  commit_email        = "pwd1390@mail.ru"
   overwrite_on_create = true
 }
